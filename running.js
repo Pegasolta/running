552471033b6a32ed79988c97db0b475c168daba0
Felix Tan <felixtanhm@gmail.com>
2017-03-29 17:06:03 +0800

    GAME WORKS

--- running.js
+++ running.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     console.log('loaded')
 
+//Background Image and Llama image storage
     var img = $('img:first')
     var imgPosition = img.position()
     var $window = $(window)
     var windowWidth = $window.width()
-    var llama = document.querySelector('#llama')
+    var llama = $('#llama')
 
     // Llama Jump Function
     $(document).on('keydown', function(e) {
@@ -13,7 +14,7 @@ $(document).ready(function() {
             $("#llama").addClass("jumped")
         }
     }).on('keyup', function() {
-      // if (llama top is 13%) {}
+        // if (llama top is 13%) {}
         $('#llama').removeClass('jumped')
     })
 
@@ -26,12 +27,33 @@ $(document).ready(function() {
     }
     setInterval(movingBackground, 1000 / 50)
 
+    // Obstacle Generator
+    function obstacleMaker () {
+        var obstacle = $("#backgroundDiv").append("<img src='assets/obstacles/_rocks/stone01.png' class='obstacle'>")
+    }
+    setInterval(obstacleMaker, Math.floor(Math.random() * (2500 - 1000)) + 1000)
+
+    // Moving speed of all Obstacles
+    function obstacleAnim () {
+        $(".obstacle").animate({
+            left: "-=25"
+        }, 50)
+
+        //Collission of Obstacles with Llama
+        var obstacleLeft = parseInt($(".obstacle").css("left"))
+        var llamaRight = parseInt($("#llama").css("left")) + parseInt($("#llama").css("width"))
+
+        var obstacleHeight = parseInt($(".obstacle").css("top"))
+        var llamaHeight = parseInt($("#llama").css("top")) + parseInt($("#llama").css("height"))
+
+        if (obstacleLeft < 0) {
+            $("img:nth-child(3)").remove()
+        }
+        else if (obstacleLeft < llamaRight && llamaHeight > obstacleHeight) {
+            $("img:nth-child(3)").remove()
+            $(".losingScreen").css("display", "block")
+            $("#backgroundDiv").css("display", "none")
+        }
+    }
+    setInterval(obstacleAnim, 1)
 })
-
-
-// Constructor specifies base properties for Rock
-function rock(height, width, multiplier) {
-  this.height = height
-  this.width = width
-  this.multiplier = multiplier
-}
