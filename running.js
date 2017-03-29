3d9af1a48f8acb42ac2dd609b68b8c306d3a3d6a
Felix Tan <felixtanhm@gmail.com>
2017-03-29 09:41:20 +0800

    llama jumps

--- running.js
+++ running.js
@@ -7,34 +7,18 @@ $(document).ready(function() {
     var windowWidth = $window.width()
     var llama = document.querySelector('#llama')
 
-    // document.addEventListener("keydown", llamaJump, false)
+    // Llama Jump Function
     $(document).on('keydown', function(e) {
         if (e.keyCode === 32) {
-            $('#llama').addClass('jumped')
+            $("#llama").addClass("jumped")
         }
     }).on('keyup', function() {
+      // if (llama top is 13%) {}
         $('#llama').removeClass('jumped')
     })
-    //    var timeoutId = setTimeOut(llamaGround, 1000)
-    //   function llamaGround () {
-    //      $('#llama').removeClass('jumped')
-    //   }
-    //
-    // })
-
-    // Llama Jump Function
-    // function llamaJump(e) {
-    //   console.log("jump function runs");
-    //     console.log("llama jumped");
-    //     llama.classList.add('jumped')
-    //     // llama.style.top = 32 + "%"
-    // }
-
-    // console.log(windowWidth);
-    // console.log("left is: " + imgPosition.left)
 
     // Moving Background Function and Interval
-    function movingBackground () {
+    function movingBackground() {
         img.css('left', imgPosition.left -= 10)
         if (imgPosition.left < -windowWidth / 2.2) {
             img.css('left', imgPosition.left = 0)
@@ -43,3 +27,11 @@ $(document).ready(function() {
     setInterval(movingBackground, 1000 / 50)
 
 })
+
+
+// Constructor specifies base properties for Rock
+function rock(height, width, multiplier) {
+  this.height = height
+  this.width = width
+  this.multiplier = multiplier
+}
